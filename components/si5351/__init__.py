78c2becd23b9f71a977c430a3ece9d8b932dd83c
Vincent <96040709+Jasionf@users.noreply.github.com>
2026-01-20 15:21:38 +0800

    Update __init__.py

--- components/si5351/__init__.py
+++ components/si5351/__init__.py
@@ -1,8 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_RESET_PIN
 from esphome import pins
+
 
 CODEOWNERS = ["@Vincent"]
 DEPENDENCIES = ["i2c"]
