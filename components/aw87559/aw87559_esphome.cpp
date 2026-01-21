bd0d980f5b1bbe2e88897b77c64f1296103abf1c
“Jasionf” <2248831014@qq.com>
2026-01-21 15:50:42 +0800

    modif

--- components/aw87559/aw87559_esphome.cpp
+++ components/aw87559/aw87559_esphome.cpp
@@ -25,22 +25,23 @@ void AW87559Component::setup() {
   ESP_LOGD(TAG, "Setting up AW87559...");
 
   // 读取芯片 ID 寄存器 0x00，应返回 0x5A
-  uint8_t id;
+  // uint8_t id;
 
   AW87559_ERROR_FAILED(this->write_byte(AW87559_REG_CHIPID, 0xff));
-  if (!this->read_reg(AW87559_REG_CHIPID, &id)) {
-    ESP_LOGE(TAG, "AW87559 not responding - read ID failed at address 0x%02X", this->address_);
-    this->mark_failed();
-    return;
-  }
+  // if (!this->read_reg(AW87559_REG_CHIPID, &id)) {
+  //   ESP_LOGE(TAG, "AW87559 not responding - read ID failed at address 0x%02X", this->address_);
+  //   this->mark_failed();
+  //   return;
+  // }
+
   AW87559_ERROR_FAILED(this->write_byte(AW87559_REG_SYSCTRL, 0x78));
 
   // 启用功率放大器（PA）：写 0x78 到系统控制寄存器 0x01
-  if (!this->write_reg(AW87559_REG_SYSCTRL, 0x78)) {
-    ESP_LOGE(TAG, "Failed to enable AW87559 PA");
-    this->mark_failed();
-    return;
-  }
+  // if (!this->write_reg(AW87559_REG_SYSCTRL, 0x78)) {
+  //   ESP_LOGE(TAG, "Failed to enable AW87559 PA");
+  //   this->mark_failed();
+  //   return;
+  // }
 
   ESP_LOGI(TAG, "AW87559 Audio Amplifier initialized @0x%02X", this->address_);
 
